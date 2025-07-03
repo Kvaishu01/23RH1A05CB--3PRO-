3678289d6c5a2cfa46b9c8882c4b35e0b59386e2
Kvaishu01 <koyyadavaishnavi40@gmail.com>
2025-07-03 16:14:42 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,13 @@
+let arr=[1,2,3,4,5,6];
+console.log(arr);
+// for(let val of arr){
+//     //console.log(val);
+//     if(val%2==0){
+//         console.log(val);
+//     }
+
+// }
+arr.push(30);
+console.log(arr);
+arr.pop(30);
+console.log(arr);
---
+++ object.js
@@ -0,0 +1,3 @@
+let user={name:"gayu",dept:"mba",age:35};
+console.log(user);
+console.log(user.age);
